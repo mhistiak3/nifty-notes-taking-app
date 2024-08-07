fc0292a1bb61d1c5a713897ef2b14cace8e1b0c6
Istiak <mhistiak2@gmail.com>
2024-08-07 19:22:19 +0600

    fixed error page and 404 page bug

--- middleware/common/errorHandler.js
+++ middleware/common/errorHandler.js
@@ -1,8 +1,10 @@
 const notFoundHandler = ( req, res, next) => {
+  res.locals.title = "Nifty - 404 Page"
   res.render("404");
 };
 
 const errorHandler = (err, req, res, next) => {
+    res.locals.title = "Nifty - Error Page";
   res.render("error", {
     error: err,
   });
